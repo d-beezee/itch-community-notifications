c7ad058b009370bef59f2ba6f2d2eee0118c3167
Davide Bizzi <davide.bizzi@unguess.io>
2024-06-05 16:56:07 +0200

    feat: Add logger and options

---
+++ src/Notifier/NotifierFactory.ts
@@ -0,0 +1,21 @@
+import { Notifier } from ".";
+import { ConsoleLogNotifier } from "./ConsoleLogNotifier";
+import { N8NNotifier } from "./N8NNotifier";
+
+const notifierTypes: Record<string, new () => Notifier> = {
+  n8n: N8NNotifier,
+  console: ConsoleLogNotifier,
+};
+
+export class NotifierFactory {
+  static createNotifier(type: string): Notifier {
+    if (type in notifierTypes === false)
+      throw new Error(`Notifier type ${type} not found`);
+
+    return new notifierTypes[type]();
+  }
+}
+
+const options = Object.keys(notifierTypes);
+
+export { options };
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,46 @@
+import { Option, program } from "commander";
 import { Games } from "./Games";
-import { N8NNotifier } from "./Notifier/N8NNotifier";
+import { Logger } from "./Logger";
+import { NotifierFactory, options } from "./Notifier/NotifierFactory";
+
+program
+  .addOption(
+    new Option("-n, --notifier [notifierType]", "the notifier to use")
+      .choices(options)
+      .default("n8n")
+  )
+  .addOption(
+    new Option("-v, --verbose", "output extra debugging").default(false)
+  )
+  .parse();
+
+const { notifier, verbose } = program.opts();
+
 const dotenv = require("dotenv");
 dotenv.config();
 
+const logger = new Logger({ verbose });
+
 const main = async () => {
+  logger.log("Starting: ");
+  logger.log(`Options: ${JSON.stringify(program.opts())}`);
   const games = new Games();
+  logger.log("Games instance created");
   await games.init();
+  logger.log("Games initialized");
 
   const withCopies = games.getGamesObject(games.withCommunityCopies);
+  logger.log("Games with community copies retrieved");
+  logger.log(`Games with community copies: ${JSON.stringify(withCopies)}`);
 
-  const notifer = new N8NNotifier();
-  await notifer.notify(withCopies);
+  logger.log(`Notifiying with ${notifier}`);
+  await NotifierFactory.createNotifier(notifier).notify(withCopies);
 
   const gamesWithoutCopies = games.getGamesObject(games.withoutCommunityCopies);
+  logger.log("Games without community copies retrieved");
+  logger.log(
+    `Games without community copies: ${JSON.stringify(gamesWithoutCopies)}`
+  );
 
   await games.updateGames(gamesWithoutCopies);
 };
